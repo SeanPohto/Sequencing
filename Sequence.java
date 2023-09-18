ce017c8318240a321d38ccc54bb9481c0625323c
Sean Pohto <sean.pohto@mrjaffesclass.com>
2023-09-18 11:38:56 -0700

    Commit message

--- Sequence.java
+++ Sequence.java
@@ -1,6 +1,31 @@
-public class Sequence
+public class Sequence 
 {
-    public static void main(String[] args) {
-        
+    int a = 5;
+    int b = 2;
+    int c = 1;
+    int d = -8;
+
+    public void run() {
+        System.out.println("HelloWorld");
+        if (a == 5) {
+            System.out.println("Success");
+        } else {
+            System.out.println("Failure");
+        }
+        if (b == 2) {
+            System.out.println("Success");
+        } else {
+            System.out.println("Failure");
+        }
+        if (c != 1) {
+            System.out.println("Success");
+        } else {
+            System.out.println("Failure");
+        }
+        if (d == -8 && d==4) {
+            System.out.println("Success");
+        } else {
+            System.out.println("Failure");
+        }
     }
 }
