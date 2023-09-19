88dfa72448464243afc169f9b62c08ae1094324d
Sean Pohto <sean.pohto@mrjaffesclass.com>
2023-09-19 11:30:33 -0700

    Commit message

---
+++ ForLoops.java
@@ -0,0 +1,14 @@
+public class ForLoops
+{
+    public void run() {
+        for (int i = 0; i <= 15; i++) { 
+            System.out.println("step 1 " + i);
+        }
+        for (int j = 10; j >= 0; j -= 2) {
+            System.out.println("step 2 " + j);
+        }
+        for (int k = 10; k >= 0; k -= 3) {
+            System.out.println("step 3 "+ k);
+        }
+    }
+}
--- Sequence.java
+++ Sequence.java
@@ -44,5 +44,6 @@ public class Sequence
             a++;
             b++;
         }
+
     }
 }
